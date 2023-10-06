7ed63803911d8b290bf126496b9454a55a77623e
avaniviramgama133 <143678455+avaniviramgama133@users.noreply.github.com>
2023-10-06 08:57:36 +0530

    Add files via upload

---
+++ TASK-2/script-12.php
@@ -0,0 +1,9 @@
+<!-- Write a PHP script to concatenate two strings and display the combined result. -->
+<?php
+$string1 = "AVANI ";
+$string2 = "VIRAMGAMA";
+
+$combined = $string1 . $string2;
+
+echo $combined;
+?>
