24c542a021c05c463577c85b93650dc8e6c05516
avaniviramgama133 <143678455+avaniviramgama133@users.noreply.github.com>
2023-10-28 13:09:55 +0530

    Update array_14.php

--- Array/array_14.php
+++ Array/array_14.php
@@ -1,9 +1,9 @@
 <?php
 
 $personal_detail = array(
-    "name"=>"meru",
+    "name"=>"ani",
     "age"=>"22",
-    "email"=>"morimeru0@gmail.com");
+    "email"=>"ani1@gmail.com");
 unset($personal_detail["email"]);
 print_r($personal_detail)
 ?>
