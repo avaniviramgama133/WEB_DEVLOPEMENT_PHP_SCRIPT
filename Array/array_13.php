07172c45b0a142b2851937b06f7b7c11f29fa97c
avaniviramgama133 <143678455+avaniviramgama133@users.noreply.github.com>
2023-10-28 13:09:35 +0530

    Update array_13.php

--- Array/array_13.php
+++ Array/array_13.php
@@ -1,7 +1,7 @@
 <?php
 $personal_detail = array(
-    "name"=>"meru",
+    "name"=>"avi",
     "age"=>"22",
-    "email"=>"morimeru0@gmail.com");
+    "email"=>"avani0@gmail.com");
 echo "The age of the person is ".$personal_detail["age"];
 ?>
